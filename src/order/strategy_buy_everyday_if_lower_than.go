45a34d78e0b147c50ec7bb0bdad638858a5a8d43
Sungguk Lim <limasdf@gmail.com>
2023-06-25 00:57:51 +0900

    Add buy lower than price strategy

---
+++ src/order/strategy_buy_everyday_if_lower_than.go
@@ -0,0 +1,53 @@
+package order
+
+import (
+	"strconv"
+	"time"
+
+	"github.com/go-co-op/gocron"
+	"sunglim.github.com/sunglim/log"
+	"sunglim.github.com/sunglim/order/koreainvestment"
+)
+
+// Buy a stock if the price is lower than ...
+
+func buyLowerOrder(codePrices []CodePrice, logger *log.Logger) {
+	for _, codePrice := range codePrices {
+		currentPrice := koreainvestment.ApiInqueryPrice{}.Call(codePrice.Code)
+		priceInt, _ := strconv.Atoi(currentPrice)
+		if priceInt > codePrice.Price {
+			continue
+		}
+		logger.Println("buy lower than is triggerd", "code", codePrice.Code, "orderprice", priceInt)
+		BuyLowerOrderCash(codePrice.Code, logger)
+	}
+
+}
+
+func BuyLowerOrderCash(code string, logger *log.Logger) {
+	response := koreainvestment.CreateApiOrderCash(code).Call()
+	handleResponse(response)
+	if !response.IsSuccess() {
+		logger.Printf("Getting Api order cash failed from the strategry")
+		logger.Printf("Error[%s]", response.Msg1)
+		return
+	}
+
+	logger.Printf("An order is successfully sent [%v]", response)
+}
+
+type CodePrice struct {
+	Code  string
+	Price int
+}
+
+func StrategryBuyEveryDayIfLowerThan(buytime string, codePrices []CodePrice) {
+	logger := log.Default()
+	logger.SetPrefix("[Buy if average is lower than] ")
+
+	logger.Println("start new stragegy")
+
+	s := gocron.NewScheduler(time.Now().Location()).Every(1).Day().At(buytime)
+	s.Do(buyLowerOrder, codePrices, logger)
+	s.StartAsync()
+}
